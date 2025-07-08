74999348b189470642459217834a334ca2975c16
ArchILLtect <nickhansonsr@gmail.com>
2025-07-08 12:50:29 -0500

    Add: panner and tracker pages

---
+++ src/pages/BudgetTracker.jsx
@@ -0,0 +1,20 @@
+
+import { Box, Heading, Center } from '@chakra-ui/react'
+
+
+function BudgetTracker() {
+
+  return (
+    <Box bg="gray.200" p={4} minH="100vh">
+      <Box p={4} maxW="800px" mx="auto" borderWidth={1} borderRadius="lg" boxShadow="md" background={"white"}>
+        {/* <pre>{JSON.stringify(income, null, 2)}</pre>
+        <pre>{JSON.stringify(expenses, null, 2)}</pre> */}
+        <Center mb={4}>
+          <Heading>Budget Planner</Heading>
+        </Center>
+      </Box>
+    </Box>
+  )
+}
+
+export default BudgetTracker
