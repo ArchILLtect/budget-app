ed39f45931b6baf310562bc23360b6d3a71b1032
ArchILLtect <nickhansonsr@gmail.com>
2025-07-08 16:55:06 -0500

    Add/Update: add placeholder modal for account syncing; add accounts page and button for account sync to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,32 @@
-import { Box, Flex, HStack, Link, IconButton, useColorModeValue, Heading } from '@chakra-ui/react';
+import { Box,
+  Flex,
+  HStack,
+  Link,
+  Heading,
+  Button,
+  useColorModeValue,
+  useDisclosure
+} from '@chakra-ui/react';
 import { NavLink as RouterLink } from 'react-router-dom';
-// TODO: Add for moblie: import { HamburgerIcon } from '@chakra-ui/icons';
+import { useBudgetStore } from '../state/budgetStore';
+import AccountSyncModal from './accountSyncModal';
+
+// TODO: Add for mobile: import { HamburgerIcon } from '@chakra-ui/icons';
 
 export default function NavBar() {
+
+  const currentPage = useBudgetStore((s) => s.currentPage);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box position="sticky" top="0" zIndex="1000" bg={useColorModeValue('teal.500', 'teal.700')} px={4} py={3} shadow="md">
       <Flex h={8} alignItems={'center'} justifyContent={'space-between'}>
         <Heading size="md" color="white">Budgeting App</Heading>
 
+        {currentPage === 'accounts' && (
+          <Button onClick={onOpen}>Sync Account</Button>
+        )}
+        <AccountSyncModal isOpen={isOpen} onClose={onClose} />
         <HStack as="nav" spacing={6}>
           <Link
             as={RouterLink}
@@ -20,6 +39,18 @@ export default function NavBar() {
             color="white"
           >
             Planner
+          </Link>
+          <Link
+            as={RouterLink}
+            to="/accounts"
+            px={3}
+            py={2}
+            rounded="md"
+            _hover={{ bg: 'teal.600', color: 'white' }}
+            _activeLink={{ bg: 'teal.700', fontWeight: 'bold', color: 'white' }}
+            color="white"
+          >
+            Accounts
           </Link>
           <Link
             as={RouterLink}
