f2d64f872313465811e2e2c4bd54dae303d1f855
ArchILLtect <nickhansonsr@gmail.com>
2025-07-08 12:48:42 -0500

    Upgrade: switch to React Router and use pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import { useBudgetStore } from './state/budgetStore'
-import { Box, Heading, Center } from '@chakra-ui/react'
-import IncomeCalculator from './components/IncomeCalculator'
-import ExpenseTracker from './components/ExpenseTracker'
-import ExpensePie from './components/ExpensePie'
 
-function App() {
+import { ChakraProvider, Box } from '@chakra-ui/react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import BudgetPlanner from './pages/BudgetPlanner'
+import BudgetTracker from './pages/BudgetTracker'
+import Navbar from './components/Navbar'
 
+export default function App() {
   return (
-    <Box bg="gray.100" minH="100vh">
-      <Center>
-        <Heading m={4}>Budget App</Heading>
-      </Center>
-      <Box p={4} maxW="800px" mx="auto" borderWidth={1} borderRadius="lg" boxShadow="md" background={"white"}>
-        {/* <pre>{JSON.stringify(income, null, 2)}</pre>
-        <pre>{JSON.stringify(expenses, null, 2)}</pre> */}
-        <IncomeCalculator />
-        <ExpenseTracker />
-        <ExpensePie />
-      </Box>
-    </Box>
+    <ChakraProvider>
+      <Router>
+        <Box>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<BudgetPlanner />} />
+            <Route path="/planner" element={<BudgetPlanner />} />
+            <Route path="/tracker" element={<BudgetTracker />} />
+          </Routes>
+        </Box>
+      </Router>
+    </ChakraProvider>
   )
 }
-
-export default App
