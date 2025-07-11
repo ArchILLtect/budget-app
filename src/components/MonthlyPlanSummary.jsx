3e80bfe5bafa991b06205266fbc9be85feeb8372
ArchILLtect <nickhansonsr@gmail.com>
2025-07-10 21:11:39 -0500

    Refactor: remove parameter/arg carry-over and direct access to store

--- src/components/MonthlyPlanSummary.jsx
+++ src/components/MonthlyPlanSummary.jsx
@@ -1,13 +1,18 @@
 import { Box, Heading, Stat, StatGroup, Text,
   StatLabel, StatNumber, useColorModeValue,
   Flex} from "@chakra-ui/react";
+import { useBudgetStore } from '../state/budgetStore';
 import dayjs from "dayjs";
 
-export default function MonthlyPlanSummary({ plan }) {
+export default function MonthlyPlanSummary() {
+
+  const selectedMonth = useBudgetStore(s => s.selectedMonth);
+  const plan = useBudgetStore(s => s.monthlyPlans[selectedMonth]);
+
   if (!plan) return null;
 
   return (
-    <Box px={4} py={3} boxShadow="md" borderBottomRadius="lg" bg={useColorModeValue('white', 'gray.700')}>
+    <Box px={4} py={3} boxShadow="md" bg={useColorModeValue('white', 'gray.700')}>
       <Flex justifyContent="space-between" alignItems="center" mb={3}>
         <Heading size="md" mb={2}>Plan Summary</Heading>
         {plan.createdAt && (
@@ -23,6 +28,11 @@ export default function MonthlyPlanSummary({ plan }) {
             <StatNumber color="teal.600">${plan.netIncome?.toFixed(2)}</StatNumber>
         </Stat>
 
+        <Stat textAlign={'center'}>
+            <StatLabel>Total Expenses</StatLabel>
+            <StatNumber color="teal.600">${plan.totalExpenses?.toFixed(2)}</StatNumber>
+        </Stat>
+
         {plan.totalSavings > 0 && (
         <Stat textAlign={'center'}>
             <StatLabel>Total Savings</StatLabel>
@@ -31,14 +41,9 @@ export default function MonthlyPlanSummary({ plan }) {
         )}
 
         <Stat textAlign={'center'}>
-            <StatLabel>Total Expenses</StatLabel>
-            <StatNumber color="teal.600">${plan.totalExpenses?.toFixed(2)}</StatNumber>
-        </Stat>
-
-        <Stat textAlign={'center'}>
             <StatLabel>Leftover</StatLabel>
             <StatNumber color={plan.totalSavings >= 0 ? "green.600" : "red.600"} fontSize="2xl">
-              ${plan.totalSavings}
+              ${plan.estLeftover?.toFixed(2)}
             </StatNumber>
         </Stat>
       </StatGroup>
